158cb38a70a6fac6a77f7fbd227c456013671cfb
netwarp <resethread@gmail.com>
2019-08-31 11:58:39 +0200

    update model Board

--- models/Board.js
+++ models/Board.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 const board_schema = new Schema({
-    title: String
+    title: String,
+    slug: String,
+    description: String,
+    folder: String,
+    avatar: String
 });
 
 const Board = mongoose.model('Board', board_schema);
