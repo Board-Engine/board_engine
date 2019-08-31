2c36e7377fcd4ca006c4be690fc2d4d699e40540
netwarp <resethread@gmail.com>
2019-09-01 00:09:57 +0200

    add session + cookieparser

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const flash = require('express-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const sessionStore = new session.MemoryStore;
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/board', { useNewUrlParser: true });
 
 const port = 8080;
 
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: false
 }));
+app.use(bodyParser.json());
+app.use(cookieParser('secret'));
+app.use(session({
+    cookie: { maxAge: 60000 },
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}));
+app.use(flash());
 
 const nunjucks = require('nunjucks');
 
