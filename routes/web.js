ab5516bd6e409e59934bb45733b4de5c89b2a47b
netwarp <resethread@gmail.com>
2019-09-10 10:13:32 +0200

    up routes

--- routes/web.js
+++ routes/web.js
@@ -16,10 +16,11 @@ router.get('/boards/:board_slug', ThreadsController.index);
 router.get('/boards/:board_slug/:board_id/create', ThreadsController.create);
 router.post('/boards/:board_slug/:board_id/create', ThreadsController.store);
 
-router.get('/boards/:board_slug/:thread_id', PostsController.index);
+router.get('/boards/:board_slug/:thread_slug', PostsController.index);
 router.post('/posts/:thread_id', PostsController.store);
 
 router.get('/threads', ThreadsController.index);
+router.get('/threads/:id', PostsController.index);
 
 
 module.exports = router;
