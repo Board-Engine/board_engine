429bf6cc3100902716a0926ba8b929d248a9ddd4
netwarp <resethread@gmail.com>
2019-09-16 15:15:18 +0200

    up routes

--- routes/web.js
+++ routes/web.js
@@ -6,7 +6,16 @@ const BoardsController = require('../controllers/BoardsController');
 const ThreadsController = require('../controllers/ThreadsController');
 const PostsController = require('../controllers/PostsController');
 
+const fileUpload = require('express-fileupload');
+router.use(fileUpload({
+	limits: {
+		// in MB
+		fileSize: 2 * 1024 * 1024
+	}
+}));
+
 router.get('/', FrontController.index);
+router.get('/images/:type/:folder/:image', FrontController.image);
 
 router.get('/boards', BoardsController.index);
 router.get('/boards/create', BoardsController.create);
@@ -22,5 +31,8 @@ router.post('/posts/:thread_id', PostsController.store);
 router.get('/threads', ThreadsController.index);
 router.get('/threads/:thread_id', PostsController.index);
 
+
+
+router.get('/test', BoardsController.test)
 
 module.exports = router;
