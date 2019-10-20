167126cc2b1484b93c4fa84b2d9738666f3f0ab5
netwarp <resethread@gmail.com>
2019-10-20 15:22:14 +0200

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const methodOverride = require('method-override');
 const router = express.Router();
 const AdminController = require('../controllers/Admin/AdminController');
 const BoardsController = require('../controllers/Admin/BoardsController');
+const ThreadsController = require('../controllers/Admin/ThreadsController');
 
 
 router.use(methodOverride('_method'))
@@ -13,5 +14,7 @@ router.get('/boards', BoardsController.index);
 router.get('/boards/:id', BoardsController.edit);
 router.post('/boards/:id', BoardsController.update);
 router.delete('/boards/:id', BoardsController.destroy);
+
+router.get('/threads', ThreadsController.index);
 
 module.exports = router;
