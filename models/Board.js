7dea2ec18c5c56b4aee3700b8f4f06fe7da018c9
netwarp <resethread@gmail.com>
2019-11-12 19:29:39 +0100

    Update Board.js

--- models/Board.js
+++ models/Board.js
@@ -1,3 +1,45 @@
+const config = require('../env');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+const Board = sequelize.define('board', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+    },
+    folder: {
+        type: Sequelize.STRING,
+    },
+    image_path: {
+        type: Sequelize.STRING,
+    },
+    ip: {
+        type: Sequelize.STRING,
+    },
+
+    createdAt: {type: Sequelize.DATE, field: 'created_at'},
+    updatedAt: {type: Sequelize.DATE, field: 'updated_at'},
+}, {
+    sequelize,
+    modelName: 'Board'
+});
+
+module.exports = Board;
+/*
 const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
@@ -26,3 +68,5 @@ schema.plugin(mongoosePaginate);
 const Board = mongoose.model('Board', schema);
 
 module.exports = Board;
+
+ */
