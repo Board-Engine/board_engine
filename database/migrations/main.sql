8f74f91cb12bdac4cc59bb236b12a9a1b7a05178
netwarp <resethread@gmail.com>
2019-11-12 16:07:16 +0100

    update main

--- database/migrations/main.sql
+++ database/migrations/main.sql
@@ -1,29 +1,29 @@
-# --------------------------------------------------------------------
-#
-# WORKS ON POSTGRES ONLY !
-#
-#--------------------------------------------------------------------
+/* --------------------------------------------------------------------
+*
+* WORKS ON POSTGRES ONLY !
+*
+--------------------------------------------------------------------*/
 
 
-# --------------------------------------------------------------------
-#
-# TABLE users
-#
-#--------------------------------------------------------------------
+/* --------------------------------------------------------------------
+*
+* TABLE users
+*
+--------------------------------------------------------------------*/
 CREATE TABLE IF NOT EXISTS users(
 	id INT NOT NULL PRIMARY KEY
 );
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS name VARCHAR(50);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS password VARCHAR(255);
+ALTER TABLE users ADD COLUMN IF NOT EXISTS name VARCHAR(50);
+ALTER TABLE users ADD COLUMN IF NOT EXISTS password VARCHAR(255);
 
 
 
 
-# --------------------------------------------------------------------
-#
-# TABLE boards
-#
-#--------------------------------------------------------------------
+/* --------------------------------------------------------------------
+*
+* TABLE boards
+*
+--------------------------------------------------------------------*/
 CREATE TABLE IF NOT EXISTS boards(
 	id INT NOT NULL PRIMARY KEY
 );
@@ -41,63 +41,71 @@ ALTER TABLE boards ADD COLUMN IF NOT EXISTS updated_at DATE DEFAULT CURRENT_DATE
 
 
 
-# --------------------------------------------------------------------
-#
-# TABLE threads
-#
-#--------------------------------------------------------------------
+/* --------------------------------------------------------------------
+*
+* TABLE threads
+*
+--------------------------------------------------------------------*/
 CREATE TABLE IF NOT EXISTS threads(
 	id INT NOT NULL PRIMARY KEY
 );
 
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS title VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS slug VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS content VARCHAR(255);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS folder VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS avatar VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS image_path VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS ip VARCHAR(150);
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS title VARCHAR(150);
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS slug VARCHAR(150);
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS content VARCHAR(255);
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS folder VARCHAR(150);
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS avatar VARCHAR(150);
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS image_path VARCHAR(150);
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS ip VARCHAR(150);
 
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS board_id INT NOT NULL REFERENCES boards (id);
+/*ALTER TABLE threads ADD COLUMN IF NOT EXISTS board_id INT NOT NULL REFERENCES boards (id);*/
+ALTER  TABLE threads ADD COLUMN IF NOT EXISTS board_id INT NOT NULL;
+ALTER TABLE threads DROP CONSTRAINT IF EXISTS board_id;
+ALTER TABLE threads ADD CONSTRAINT board_id FOREIGN KEY (board_id) REFERENCES boards (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS created_at DATE DEFAULT CURRENT_DATE;
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS updated_at DATE DEFAULT CURRENT_DATE;
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS created_at DATE DEFAULT CURRENT_DATE;
+ALTER TABLE threads ADD COLUMN IF NOT EXISTS updated_at DATE DEFAULT CURRENT_DATE;
 
 
 
 
-# --------------------------------------------------------------------
-#
-# TABLE posts
-#
-#--------------------------------------------------------------------
+/* --------------------------------------------------------------------
+*
+* TABLE posts
+*
+--------------------------------------------------------------------*/
 CREATE TABLE IF NOT EXISTS posts(
 	id INT NOT NULL PRIMARY KEY
 );
 
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS title VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS slug VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS content VARCHAR(255);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS folder VARCHAR(150);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS avatar VARCHAR(150);
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS title VARCHAR(150);
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS slug VARCHAR(150);
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS content VARCHAR(255);
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS folder VARCHAR(150);
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS avatar VARCHAR(150);
 
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS board_id INT NOT NULL REFERENCES boards (id);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS thread_id INT NOT NULL REFERENCES threads (id);
+ALTER  TABLE posts ADD COLUMN IF NOT EXISTS board_id INT NOT NULL;
+ALTER TABLE posts DROP CONSTRAINT IF EXISTS board_id;
+ALTER TABLE posts ADD CONSTRAINT board_id FOREIGN KEY (board_id) REFERENCES boards (id) ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS created_at DATE DEFAULT CURRENT_DATE;
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS updated_at DATE DEFAULT CURRENT_DATE;
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS thread_id INT NOT NULL;
+ALTER TABLE posts DROP CONSTRAINT IF EXISTS thread_id;
+ALTER TABLE posts ADD CONSTRAINT thread_id FOREIGN KEY (thread_id) REFERENCES threads (id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS created_at DATE DEFAULT CURRENT_DATE;
+ALTER TABLE posts ADD COLUMN IF NOT EXISTS updated_at DATE DEFAULT CURRENT_DATE;
 
 
-# --------------------------------------------------------------------
-#
-# TABLE ip_ban
-#
-#--------------------------------------------------------------------
+/* --------------------------------------------------------------------
+*
+* TABLE ip_ban
+*
+--------------------------------------------------------------------*/
 CREATE TABLE IF NOT EXISTS ip_ban(
 	id INT NOT NULL PRIMARY KEY
 );
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS ip VARCHAR(25);
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS reason VARCHAR(25);
+ALTER TABLE ip_ban ADD COLUMN IF NOT EXISTS ip VARCHAR(25);
+ALTER TABLE ip_ban ADD COLUMN IF NOT EXISTS reason VARCHAR(25);
 
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS created_at DATE DEFAULT CURRENT_DATE;
-ALTER TABLE boards ADD COLUMN IF NOT EXISTS updated_at DATE DEFAULT CURRENT_DATE;	
+ALTER TABLE ip_ban ADD COLUMN IF NOT EXISTS created_at DATE DEFAULT CURRENT_DATE;
+ALTER TABLE ip_ban ADD COLUMN IF NOT EXISTS updated_at DATE DEFAULT CURRENT_DATE;
