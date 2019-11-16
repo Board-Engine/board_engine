4f7d5087bc097de04647575e70c06a240cae4639
netwarp <resethread@gmail.com>
2019-11-16 21:56:45 +0100

    1st test

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+const Board = require('../models/Board');
+const Thread = require('../models/Thread');
+const Post = require('../models/Post');
+
+test('true to be true', () => {
+    expect(true).toBeTruthy ();
+});
